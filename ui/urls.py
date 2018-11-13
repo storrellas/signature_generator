00e639e7bba3882a37fe7c311cb3a5caa4e92ef9
storrellas <storrellas@gmail.com>
2018-11-13 08:45:08 +0000

    ENH: Initial commit

---
+++ ui/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+
+from . import views
+from django.views.generic import TemplateView
+
+# Set URL patterns
+urlpatterns = []
+urlpatterns += url('', TemplateView.as_view(template_name="signature.html"), name='signature'),
