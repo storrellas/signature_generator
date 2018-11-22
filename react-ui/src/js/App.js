ea82306ed64f291162916d9c61b5a70d491a388f
storrellas <storrellas@gmail.com>
2018-11-22 15:55:34 +0000

    ENH: Adding configurations for react

--- react-ui/src/js/App.js
+++ react-ui/src/js/App.js
@@ -5,7 +5,7 @@ class App extends Component{
   render(){
     return(
       <div className="App">
-        <h1> Hello, World! From Sergiaasfasdf dsafds</h1>
+        <h1>This is React Generating Content</h1>
       </div>
     );
   }
