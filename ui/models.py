d1cce52a18297e438117e74e3d7d376a6c87bc06
storrellas <storrellas@gmail.com>
2018-12-05 07:43:53 +0000

    ENH: Generated models

--- ui/models.py
+++ ui/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class SignatureVistor(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    mail = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.mail
